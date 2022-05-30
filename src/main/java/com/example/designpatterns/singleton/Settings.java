1776be91b61926da9e59f23811b0d380d05aa686
elther <elther@naver.com>
2022-05-30 22:26:43 +0900

    practice : singleton pattern, eager init

--- src/main/java/com/example/designpatterns/singleton/Settings.java
+++ src/main/java/com/example/designpatterns/singleton/Settings.java
@@ -4,7 +4,7 @@ import java.util.Set;
 
 public class Settings {
 
-    private static Settings instance;
+    private static final Settings INSTANCE = new Settings();
 
     private Settings(){
 
@@ -12,9 +12,6 @@ public class Settings {
 
     // synchronized 성능에 별로 좋지 않다..
     public static synchronized Settings getInstance(){
-        if(instance == null){
-            instance = new Settings();
-        }
-        return instance;
+        return INSTANCE;
     }
 }
