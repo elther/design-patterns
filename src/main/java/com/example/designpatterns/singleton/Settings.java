957f073d6df1b1366fe7756d8d3a03fe45c07c29
elther <elther@naver.com>
2022-05-30 22:29:15 +0900

    practice : singleton pattern, double checked locking

--- src/main/java/com/example/designpatterns/singleton/Settings.java
+++ src/main/java/com/example/designpatterns/singleton/Settings.java
@@ -2,12 +2,19 @@ package com.example.designpatterns.singleton;
 
 public class Settings {
 
-    private static final Settings INSTANCE = new Settings();
+    private static Settings instance;
 
     private Settings(){ }
 
     // synchronized 성능에 별로 좋지 않다..
     public static Settings getInstance(){
-        return INSTANCE;
+        if(instance == null){
+            synchronized (Settings.class){
+                if(instance == null){
+                    instance = new Settings();
+                }
+            }
+        }
+        return instance;
     }
 }
