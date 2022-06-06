b05462eb768217829873f6c8e52ccc2e4b663ad7
elther <elther@naver.com>
2022-06-06 22:23:09 +0900

    practice : singleton pattern, enum

---
+++ src/main/java/com/example/designpatterns/factorymethod/BlackShip.java
@@ -0,0 +1,10 @@
+package com.example.designpatterns.factorymethod;
+
+public class BlackShip extends Ship{
+
+    public BlackShip() {
+        setName("blackship");
+        setLogo("\uD83D\uDEE5");
+        setColor("black");
+    }
+}
---
+++ src/main/java/com/example/designpatterns/factorymethod/WhiteShip.java
@@ -0,0 +1,10 @@
+package com.example.designpatterns.factorymethod;
+
+public class WhiteShip extends Ship{
+
+    public WhiteShip() {
+        setName("whiteship");
+        setLogo("\uD83D\uDEE5");
+        setColor("white");
+    }
+}
