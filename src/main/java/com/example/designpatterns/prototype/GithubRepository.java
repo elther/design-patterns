cbb748e11d63a390dd6ed87dc3f761b11dd0e3e6
elther <elther@naver.com>
2022-06-07 22:23:35 +0900

    practice : prototype

---
+++ src/main/java/com/example/designpatterns/prototype/GithubRepository.java
@@ -0,0 +1,22 @@
+package com.example.designpatterns.prototype;
+
+public class GithubRepository {
+    String User;
+    String Name;
+
+    public String getUser() {
+        return User;
+    }
+
+    public void setUser(String user) {
+        User = user;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+}
